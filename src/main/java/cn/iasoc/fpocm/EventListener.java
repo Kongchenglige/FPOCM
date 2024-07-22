455e434ece0919996b5650027d20ecaff9b08d5f
Lily Ellenvia <kongchenglige@gmail.com>
2024-07-23 04:11:37 +0800

    clean the code

--- src/main/java/cn/iasoc/fpocm/EventListener.java
+++ src/main/java/cn/iasoc/fpocm/EventListener.java
@@ -6,10 +6,9 @@ import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.SmithItemEvent;
+import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -30,6 +29,7 @@ public class EventListener implements Listener {
             event.getWhoClicked().sendMessage("§c[FPOCM] 你不能合成盾牌！");
             event.setCancelled(true);
         }
+        modifyItem(item);
     }
     @EventHandler
     public void onEntityPickupItem(EntityPickupItemEvent event) {
@@ -41,41 +41,7 @@ public class EventListener implements Listener {
             // Cancel the event
             event.setCancelled(true);
         }
-        // If item is axe, set damage to 1
-        if (item.getType().toString().endsWith("AXE")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if(!meta.toString().contains("attribute-modifiers")){
-                //((Logger) LogManager.getRootLogger()).info("Modify item: " + meta);
-                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(),"generic.attackDamage", 1.0, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
-                item.setItemMeta(meta);
-            }
-        }
-        // If item is sword, add 310% knock back distance
-        if (item.getType().toString().endsWith("SWORD")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if(!meta.toString().contains("attribute-modifiers")) {
-                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(),"generic.knockback", 3.1, AttributeModifier.Operation.MULTIPLY_SCALAR_1, EquipmentSlot.HAND);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_KNOCKBACK, modifier);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(),"generic.attackDamage", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
-                item.setItemMeta(meta);
-            }
-        }
-        // If item is netherite armor, remove knock back resistance
-        if (item.getType().toString().contains("NETHERITE")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if (isArmor(item) && !meta.hasAttributeModifiers()) {
-                meta.addAttributeModifier(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "generic.armor", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
-                meta.addAttributeModifier(Attribute.GENERIC_ARMOR_TOUGHNESS, new AttributeModifier(UUID.randomUUID(), "generic.armor_toughness", 3.0, AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
-                item.setItemMeta(meta);
-            }
-        }
+        modifyItem(item);
     }
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
@@ -101,41 +67,7 @@ public class EventListener implements Listener {
             // Cancel the event
             event.setCancelled(true);
         }
-        // If item is axe, set damage to 1
-        if (item.getType().toString().endsWith("AXE")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if(!meta.toString().contains("attribute-modifiers")){
-                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(),"generic.attackDamage", 1.0, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
-                item.setItemMeta(meta);
-            }
-        }
-        // If item is sword, add 310% knock back distance
-        if (item.getType().toString().endsWith("SWORD")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            //((Logger) LogManager.getRootLogger()).info("Crafted item: " + meta);
-            if (!meta.toString().contains("attribute-modifiers")) {
-                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.knockback", 3.1, AttributeModifier.Operation.MULTIPLY_SCALAR_1, EquipmentSlot.HAND);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_KNOCKBACK, modifier);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "generic.attackDamage", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
-                item.setItemMeta(meta);
-            }
-        }
-        // If item is netherite armor, remove knock back resistance
-        if (item.getType().toString().contains("NETHERITE")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if (isArmor(item) && !meta.hasAttributeModifiers()) {
-                meta.addAttributeModifier(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "generic.armor", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
-                meta.addAttributeModifier(Attribute.GENERIC_ARMOR_TOUGHNESS, new AttributeModifier(UUID.randomUUID(), "generic.armor_toughness", 3.0, AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
-                item.setItemMeta(meta);
-            }
-        }
+        modifyItem(item);
     }
     @EventHandler
     public void onInventoryDrag(InventoryDragEvent event) {
@@ -144,39 +76,42 @@ public class EventListener implements Listener {
             // Cancel the event
             event.setCancelled(true);
         }
-        // If item is axe, set damage to 1
-        if (item.getType().toString().endsWith("AXE")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if(!meta.toString().contains("attribute-modifiers")){
-                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(),"generic.attackDamage", 1.0, AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
-                item.setItemMeta(meta);
-            }
+        modifyItem(item);
+    }
+    @EventHandler
+    public void onInvClick(InventoryClickEvent event){
+        ItemStack item = event.getCurrentItem();
+        if (item == null) {
+            return;
         }
-        // If item is sword, add 310% knock back distance
-        if (item.getType().toString().endsWith("SWORD")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if (!meta.toString().contains("attribute-modifiers")) {
-                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.knockback", 3.1, AttributeModifier.Operation.MULTIPLY_SCALAR_1, EquipmentSlot.HAND);
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "generic.attackDamage", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
-                meta.addAttributeModifier(Attribute.GENERIC_ATTACK_KNOCKBACK, modifier);
-                item.setItemMeta(meta);
-            }
+        if (item.getType().toString().equals("SHIELD")) {
+            event.getWhoClicked().sendMessage("§c[FPOCM] 你不能使用盾牌！");
+            // Cancel the event
+            event.setCancelled(true);
         }
-        // If item is netherite armor, remove knock back resistance
-        if (item.getType().toString().contains("NETHERITE")) {
-            // Get the item's meta
-            ItemMeta meta = item.getItemMeta();
-            assert meta != null;
-            if (isArmor(item) && !meta.hasAttributeModifiers()) {
-                meta.addAttributeModifier(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "generic.armor", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
-                meta.addAttributeModifier(Attribute.GENERIC_ARMOR_TOUGHNESS, new AttributeModifier(UUID.randomUUID(), "generic.armor_toughness", 3.0, AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
-                item.setItemMeta(meta);
+        modifyItem(item);
+        modifyItem(event.getCursor());
+    }
+    @EventHandler
+    public void onInvOpen(InventoryOpenEvent event) {
+        try {
+            for (ItemStack itemStack : event.getInventory().getContents()) {
+                assert itemStack != null;
+                modifyItem(itemStack);
             }
+        } catch (Exception e) {
+            // Do nothing
+        }
+    }
+    @EventHandler
+    public void onLogin(PlayerJoinEvent event) {
+        try {
+            for (ItemStack itemStack : event.getPlayer().getInventory()) {
+                assert itemStack != null;
+                modifyItem(itemStack);
+            }
+        } catch (Exception e) {
+            // Do nothing
         }
     }
     @EventHandler
@@ -231,6 +166,41 @@ public class EventListener implements Listener {
                 return true;
             default:
                 return false;
+        }
+    }
+    private void modifyItem(ItemStack item){
+        // If item is axe, set damage to 1
+        if (item == null) return;
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null && meta.toString().contains("attribute-modifiers")){
+            return;
+        }
+        if (item.getType().toString().endsWith("AXE") && !item.getType().toString().contains("PICKAXE")) {
+            // Get the item's meta
+            AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(),"generic.attackDamage", 1.0, AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
+            assert meta != null;
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
+            item.setItemMeta(meta);
+        }
+        // If item is sword, add 310% knock back distance
+        if (item.getType().toString().endsWith("SWORD")) {
+            // Get the item's meta
+            AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.knockback", 3.1, AttributeModifier.Operation.MULTIPLY_SCALAR_1, EquipmentSlot.HAND);
+            assert meta != null;
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "generic.attackDamage", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
+            meta.addAttributeModifier(Attribute.GENERIC_ATTACK_KNOCKBACK, modifier);
+            item.setItemMeta(meta);
+        }
+        // If item is netherite armor, remove knock back resistance
+        if (item.getType().toString().contains("NETHERITE")) {
+            // Get the item's meta
+            if (isArmor(item)) {
+                AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.armor", ItemsMeta.get(item.getType().toString()), AttributeModifier.Operation.ADD_NUMBER, GetSlot(item));
+                assert meta != null;
+                meta.addAttributeModifier(Attribute.GENERIC_ARMOR, modifier);
+                meta.addAttributeModifier(Attribute.GENERIC_ARMOR_TOUGHNESS, new AttributeModifier(UUID.randomUUID(), "generic.armor_toughness", 3.0, AttributeModifier.Operation.ADD_NUMBER, GetSlot(item)));
+                item.setItemMeta(meta);
+            }
         }
     }
 }
--- src/main/java/cn/iasoc/fpocm/ItemsMeta.java
+++ src/main/java/cn/iasoc/fpocm/ItemsMeta.java
@@ -5,12 +5,10 @@ import org.bukkit.configuration.MemorySection;
 import java.util.HashMap;
 import java.util.Map;
 
-// Step 1: Define a Cache Class
 public class ItemsMeta {
     private static final Map<String, Object> cache = new HashMap<>();
 
     public void load(FPOCM plugin) {
-        // Assuming getConfig() returns a configuration object you can iterate over
         plugin.getConfig().getKeys(true).forEach(key -> {
             Object value = plugin.getConfig().get(key);
             if (value instanceof MemorySection) {
@@ -25,23 +23,3 @@ public class ItemsMeta {
     }
 
 }
-
-//public class ItemsMetas {
-//    private Map<String, Object> cache = new HashMap<>();
-//    //define armors & swords meta
-//
-//    public static final String[] armors = {
-//            "HELMET",
-//            "CHESTPLATE",
-//            "LEGGINGS",
-//            "BOOTS"
-//    };
-//    public static final String[] swords = {
-//            "WOODEN_SWORD",
-//            "STONE_SWORD",
-//            "IRON_SWORD",
-//            "GOLDEN_SWORD",
-//            "DIAMOND_SWORD",
-//            "NETHERITE_SWORD"
-//    };
-//}
